0b988e801fb033b1a3c839d8494949e0e7f75d5a
Sebastian <Sebastian@Strobl.net>
2020-03-31 21:13:23 +0200

    fetch from riot api on "/riot/{name}"

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,12 +1,8 @@
-use crate::states::APIState;
-use actix_web::{web, HttpRequest, Responder};
+use actix_web::{HttpRequest, Responder};
+use super::riot_api;
 
-pub async fn get_summoner_by_name(req: HttpRequest, data: web::Data<APIState>) -> impl Responder {
+pub async fn get_summoner_by_name(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("yasuo xd");
-    let summoner_url = format!(
-        "{}/lol/summoner/v4/summoners/by-name/{}?api_key={}",
-        &data.base_url, &name, &data.riot_api_key
-    );
-    let body = reqwest::get(&summoner_url).await.unwrap().text().await;
-    format!("data: {:?}", &body)
+    let summoner_data = riot_api::summoner_by_name(&name);
+    format!("data: {}", &summoner_data.await.unwrap())
 }
--- src/riot_api.rs
+++ src/riot_api.rs
@@ -2,12 +2,12 @@ use super::api_urls;
 use super::config;
 use super::models::Summoner;
 
-pub async fn summoner_by_name(name: &str) {
+pub async fn summoner_by_name(name: &str) -> Result<String, reqwest::Error> {
     let summoner_url = api_urls::SUMMONER_URL_BY_NAME
         .replace("repx1", api_urls::BASE_URL_EUW)
         .replace("repx2", name)
         .replace("repx3", &config::get_riot_api_key());
 
-    let body = reqwest::get(&summoner_url).await.unwrap().text().await;
-    println!("{:?}", body);
+    let summoner_data = reqwest::get(&summoner_url).await.unwrap().text().await;
+    summoner_data
 }
