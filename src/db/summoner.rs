0349103721438de7e196713f52ca7a45e5479c7c
Sebastian <Sebastian@Strobl.net>
2020-04-05 12:05:30 +0200

    fix mistake + asyncify all

--- src/db/summoner.rs
+++ src/db/summoner.rs
@@ -8,7 +8,6 @@ use crate::models::{NewSummoner, Summoner};
 use crate::schema::summoners;
 use crate::schema::summoners::dsl::summoners as all_summoners;
 
-
 /// returning all summoners
 /// it is strongly advised to not use this function in production
 /// as huge amount of data could exist
@@ -16,47 +15,53 @@ use crate::schema::summoners::dsl::summoners as all_summoners;
 /// these alternative function return a range of values
 /// but: they are not added yet:
 /// TODO: ADD RANGE FUNCTIONS
-pub fn get_all(conn: &PgConnection) -> QueryResult<Vec<Summoner>> {
+pub async fn get_all(conn: &PgConnection) -> QueryResult<Vec<Summoner>> {
     all_summoners
         .order(summoners::id.asc())
         .load::<Summoner>(conn)
 }
 
 /// returning a summoner by its id
-pub fn get_by_id(id: i64, conn: &PgConnection) -> QueryResult<Summoner> {
-    all_summoners
-        .find(id)
-        .get_result::<Summoner>(conn)
+pub async fn get_by_id(id: i64, conn: &PgConnection) -> QueryResult<Summoner> {
+    all_summoners.find(id).get_result::<Summoner>(conn)
 }
 
 /// returning only one Summoner as no summoners have the same puuid crossregional
-pub fn get_by_puuid(puuid: &str, conn: &PgConnection) -> QueryResult<Summoner> {
+pub async fn get_by_puuid(puuid: &str, conn: &PgConnection) -> QueryResult<Summoner> {
     all_summoners
         .filter(summoners::puuid.eq(puuid))
         .get_result::<Summoner>(conn)
 }
 
 /// returning Vector of Summoners as the same name can exist once per region
-pub fn get_by_name(summoner_name: &str, conn: &PgConnection) -> QueryResult<Vec<Summoner>> {
+pub async fn get_by_name(summoner_name: &str, conn: &PgConnection) -> QueryResult<Vec<Summoner>> {
     all_summoners
         .filter(summoners::name.eq(summoner_name))
         .load::<Summoner>(conn)
 }
 
 /// return only one Summoner as only one summoner per region has the same name
-pub fn get_by_name_and_region(summoner_name: &str, summoner_region: &str, conn: &PgConnection) -> QueryResult<Summoner> {
+pub async fn get_by_name_and_region(
+    summoner_name: &str,
+    summoner_region: &str,
+    conn: &PgConnection,
+) -> QueryResult<Summoner> {
     all_summoners
-        .filter(summoners::name.eq(summoner_name).and(summoners::region.eq(summoner_region)))
+        .filter(
+            summoners::name
+                .eq(summoner_name)
+                .and(summoners::region.eq(summoner_region)),
+        )
         .get_result::<Summoner>(conn)
 }
 
 /// inserts a NewSummoner or
 /// updates a Summoner
-/// 
+///
 /// avoids duplicates (uses check_existing_summoner via puuid)
-pub fn insert_summoner(new_summoner: NewSummoner, conn: &PgConnection) -> bool{
-    if check_existing_summoner(&new_summoner.puuid, conn) {
-        update_summoner(new_summoner, conn)
+pub async fn insert_summoner(new_summoner: NewSummoner, conn: &PgConnection) -> bool {
+    if check_existing_summoner(&new_summoner.puuid, conn).await {
+        update_summoner(new_summoner, conn).await
     } else {
         diesel::insert_into(summoners::table)
             .values(new_summoner)
@@ -66,10 +71,10 @@ pub fn insert_summoner(new_summoner: NewSummoner, conn: &PgConnection) -> bool{
 }
 
 /// updates summoner
-/// calling this function over insert summoner is obsolete as insert executes this function automatically 
+/// calling this function over insert summoner is obsolete as insert executes this function automatically
 /// if the summoner with the same puuid already exists
 /// some imports are only imported here as they are not required for all other functions
-pub fn update_summoner(new_summoner: NewSummoner, conn: &PgConnection) -> bool{
+pub async fn update_summoner(new_summoner: NewSummoner, conn: &PgConnection) -> bool {
     use crate::schema::summoners::dsl::{
         name as n, profile_icon_id as pi, puuid as p, revision_date as r, summoner_level as s,
     };
@@ -88,14 +93,10 @@ pub fn update_summoner(new_summoner: NewSummoner, conn: &PgConnection) -> bool{
 
 /// a helper function to check whether to update or insert a NewSummoner / to avoid duplicates
 /// using puuid instead of id as id is not known when inserting and puuid is also a unique 'identifier'
-pub fn check_existing_summoner(puuid: &str, conn: &PgConnection) -> bool {
-    diesel::select(
-        diesel::dsl::exists(
-            summoners::table.filter(
-                summoners::puuid.eq(puuid)
-            )
-        )
-    )
+pub async fn check_existing_summoner(puuid: &str, conn: &PgConnection) -> bool {
+    diesel::select(diesel::dsl::exists(
+        summoners::table.filter(summoners::puuid.eq(puuid)),
+    ))
     .get_result::<bool>(conn)
     .is_ok()
 }
--- src/db/summoner_rankeds.rs
+++ src/db/summoner_rankeds.rs
@@ -15,40 +15,38 @@ use crate::schema::summoner_rankeds::dsl::summoner_rankeds as all_summoner_ranke
 /// these alternative function return a range of values
 /// but: they are not added yet:
 /// TODO: ADD RANGE FUNCTIONS
-pub fn get_all(conn: &PgConnection) -> QueryResult<Vec<SummonerRanked>> {
+pub async fn get_all(conn: &PgConnection) -> QueryResult<Vec<SummonerRanked>> {
     all_summoner_rankeds
         .order(summoner_rankeds::id.asc())
         .load::<SummonerRanked>(conn)
 }
 
 /// returning a SummonerRanked by its summoner_id as its id is only for database management
-pub fn get_by_summoner_id(summoner_id: i64, conn: &PgConnection) -> QueryResult<SummonerRanked> {
+pub async fn get_by_summoner_id(summoner_id: i64, conn: &PgConnection) -> QueryResult<SummonerRanked> {
     all_summoner_rankeds
-        .find(summoner_id)
+        .filter(summoner_rankeds::summoner_id.eq(summoner_id))
         .get_result::<SummonerRanked>(conn)
 }
 
-
 /// insert a NewSummonerRanked oro
 /// updates a SummonerRanked
-/// 
+///
 /// avoids duplicates (uses check_existing_summoner_ranked)
-pub fn insert_summoner_ranked(new_ranked: NewSummonerRanked, conn: &PgConnection) -> bool{
-    if check_existing_summoner_ranked(new_ranked.summoner_id, conn) {
-        update_summoner_ranked(new_ranked, conn)
+pub async fn insert_summoner_ranked(new_ranked: NewSummonerRanked, conn: &PgConnection) -> bool {
+    if check_existing_summoner_ranked(new_ranked.summoner_id, conn).await {
+        update_summoner_ranked(new_ranked, conn).await
     } else {
         diesel::insert_into(summoner_rankeds::table)
             .values(new_ranked)
             .get_result::<SummonerRanked>(conn)
             .is_ok()
     }
-    
 }
 
 /// updates a SummonerRanked
 /// calling this function is obsolete as it's automatically called by the insert function
 /// if a SummonerRanked with the same summoner_id (its second unique identifier) exists
-pub fn update_summoner_ranked(new_ranked: NewSummonerRanked, conn: &PgConnection) -> bool {
+pub async fn update_summoner_ranked(new_ranked: NewSummonerRanked, conn: &PgConnection) -> bool {
     diesel::update(summoner_rankeds::table)
         .filter(summoner_rankeds::summoner_id.eq(new_ranked.summoner_id))
         .set(&new_ranked)
@@ -58,13 +56,10 @@ pub fn update_summoner_ranked(new_ranked: NewSummonerRanked, conn: &PgConnection
 
 /// a helper function to check whether to update or insert a NewSummonerRanked / to avoid duplicates
 /// using summoner_id instead of id as id is not known when inserting and summoner_id is also uinique
-pub fn check_existing_summoner_ranked(summoner_id: i64, conn: &PgConnection) -> bool {
-    diesel::select(
-        diesel::dsl::exists(
-            summoner_rankeds::table.filter(
-                summoner_rankeds::summoner_id.eq(summoner_id))
-        )
-    )
+pub async fn check_existing_summoner_ranked(summoner_id: i64, conn: &PgConnection) -> bool {
+    diesel::select(diesel::dsl::exists(
+        summoner_rankeds::table.filter(summoner_rankeds::summoner_id.eq(summoner_id)),
+    ))
     .get_result::<bool>(conn)
     .unwrap()
 }
